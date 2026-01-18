a0cd54cb50b92283b1f600ea602a788d1bb5cad2
unknown <rasyafachry.atmaja@gmail.com>
2026-01-17 20:40:42 -0800

    ini jobshet 2.3

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/about-me', [PostController::class, 'about'])->name('posts.about');
+
+use App\Http\Controllers\SekolahController;
+Route::get('/profil/{id}', [SekolahController::class, 'show']);
